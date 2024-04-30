9bb1e1933e58d4c314eed3454050a39ee92e1dec
Lars Ødegaard <lars@kodeworks.no>
2024-04-30 21:03:35 +0200

    Added solution suggestion

---
+++ create-all.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Check content: openssl x509 -noout -text -in <file>
+
+set -eE
+
+mkdir -p out/certs
+touch out/index.txt
+
+echo "Creating root key"
+openssl req \
+    -new \
+    -noenc \
+    -config root.conf \
+    -newkey rsa:4096 \
+    -keyout out/root.key \
+    -out out/root.csr
+
+echo -e "\nSelfsign root CA certificate"
+openssl ca \
+    -batch \
+    -selfsign \
+    -config root.conf \
+    -extensions ca_ext \
+    -in out/root.csr \
+    -out out/root.crt
+
+
+echo -e "\nCreating intermediate key"
+openssl req \
+    -new \
+    -noenc \
+    -config intermediate.conf \
+    -newkey rsa:4096 \
+    -keyout out/intermediate.key \
+    -out out/intermediate.csr
+
+echo -e "\nSign intermediat CA certificate"
+openssl ca \
+    -batch \
+    -config root.conf \
+    -extensions sub_ca_ext \
+    -in out/intermediate.csr \
+    -out out/intermediate.crt
+
+echo -e "\nCreate user key"
+openssl req \
+    -new \
+    -noenc \
+    -config user.conf \
+    -newkey rsa:2048 \
+    -keyout out/user.key \
+    -out out/user.csr
+
+
+echo -e "\nSign user certificate"
+openssl ca \
+    -batch \
+    -config intermediate.conf \
+    -extensions cert_ext \
+    -in out/user.csr \
+    -out out/user.crt
